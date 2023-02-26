839f6eb0d2cc1264755ad02854104fda288ae8d0
ricevillage <87378707+ricevillage@users.noreply.github.com>
2023-02-26 15:13:40 -0600

    mobile styling

--- Wordle.js
+++ Wordle.js
@@ -28,8 +28,7 @@ export class Wordle {
   async setTargetWord() {
     const wordArray = await this.getWordsFromAPI();
     const index = Math.floor(Math.random() * wordArray.length);
-    // return wordArray[index];
-    return "bunny";
+    return wordArray[index];
   }
 
   async initGame() {
